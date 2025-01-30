a6e2ffc4f000b219108dbbe4470dc84c90d1dce8
Naat <nakamura.natalia22@gmail.com>
2025-01-30 08:15:32 -0300

    Initializing repository, service, controller and routes in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/nakamuranatalia/useful-tools-api/internal/database"
+	c "github.com/nakamuranatalia/useful-tools-api/internal/controller"
+	db "github.com/nakamuranatalia/useful-tools-api/internal/database"
+	repo "github.com/nakamuranatalia/useful-tools-api/internal/repository"
 	"github.com/nakamuranatalia/useful-tools-api/internal/routes"
+	s "github.com/nakamuranatalia/useful-tools-api/internal/service"
 )
 
 func main() {
-	database.DatabaseConnection()
-	routes.HandleRequest()
+	database := db.DatabaseConnection()
 
+	repository := repo.NewRepository(database)
+	service := s.NewService(repository)
+	controller := c.NewController(service)
+	routes.HandleRequest(controller)
 }
