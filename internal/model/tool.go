ddc4d0c8535bab2356daf889135d1c43a1550de4
Naat <nakamura.natalia22@gmail.com>
2025-02-24 19:23:06 -0300

    Mapping uuid in tool model

--- internal/model/tool.go
+++ internal/model/tool.go
@@ -1,13 +1,15 @@
 package model
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Tool struct {
 	gorm.Model
-	Title       string `json:"title"`
-	Link        string `json:"link"`
-	Description string `json:"description"`
-	Tags        []Tag  `gorm:"many2many:tool_tags"`
+	Uuid        uuid.UUID `gorm:"ẗype:uuid;default:gen_random_uuid()" json:"uuid"`
+	Title       string    `json:"title"`
+	Link        string    `json:"link"`
+	Description string    `json:"description"`
+	Tags        []Tag     `gorm:"many2many:tool_tags"`
 }
