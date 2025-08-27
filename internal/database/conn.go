2c0a040f0834525cc1055e32fdf4f38a8f5e4a78
Naat <nakamura.natalia22@gmail.com>
2025-08-27 18:51:41 -0300

    Adding sql log

--- internal/database/conn.go
+++ internal/database/conn.go
@@ -20,7 +20,18 @@ const (
 func Connection() *gorm.DB {
 	dsn := fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=%s", host, user, dbName, password, sslMode)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn),
+		&gorm.Config{
+			Logger: logger.New(
+				log.New(os.Stdout, "[GORM]", log.LstdFlags),
+				logger.Config{
+					SlowThreshold:             time.Second,
+					LogLevel:                  logger.Info,
+					IgnoreRecordNotFoundError: true,
+					Colorful:                  true,
+				},
+			),
+		})
 
 	if err != nil {
 		log.Panicf("Could not start the database, error: %v", err)
